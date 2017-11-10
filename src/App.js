34b3d1c865faac79a69f13b8a5949ff15acb2c61
Matthew Choi <matthewchoi7@gmail.com>
2017-11-10 16:40:26 +1100

    adding components in semantic-ui-react-sample

--- src/App.js
+++ src/App.js
@@ -2,19 +2,18 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'semantic-ui-css/semantic.min.css';
-import Button from './Button'
+import Button from './Button';
+import HeaderAttached from './components/HeaderAttached';
 
 
 class App extends Component {
   render() {
     return (
-      <div className="App">        
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Button />
-      </div>
-    );
+    <div>
+      <HeaderAttached />
+      <Button />
+    </div>
+    )
   }
 }
 
